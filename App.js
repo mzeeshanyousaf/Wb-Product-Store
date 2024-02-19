4d5c4492b8e9c04cbddfa63a4169384af56de6a2
Tazeem <akramtazeem68@gmail.com>
2024-02-20 04:03:44 +0500

    update app js

--- App.js
+++ App.js
@@ -1,31 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 // import SignUpScreen from './SignUpScreen';
 // import RestPassword from './RestPassword';
-import LoginScreen from './LoginScreen';
-
+import LoginScreen from "./LoginScreen";
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: 'http://192.168.0.88:8080/wbpro/wbpro', 
+  uri: "http://192.168.0.88:8080/wbpro/wbpro",
   cache,
-  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+  defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
 });
-
 
 export default function App() {
   return (
     <ApolloProvider client={client}>
-     
       {/* <LoginScreen /> */}
-      <View style={{marginTop:100}}>
-        <Text>APP Interface</Text>
+      <View style={{ marginTop: 100 }}>
+        <Text>APP Interface 1</Text>
         {/* <SignUpScreen /> */}
-        <LoginScreen/>
+        <LoginScreen />
         {/* <RestPassword/> */}
-        </View>
-      <StatusBar/>
+      </View>
+      <StatusBar />
     </ApolloProvider>
   );
 }
