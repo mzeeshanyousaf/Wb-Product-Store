b2395d317814793a0192cac5058e9fe97ae70c1e
Zeeshan <chzeeshanyousaf343@gmail.com>
2024-02-21 01:55:50 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,3 @@
-// import { StatusBar } from "expo-status-bar";
-// import { StyleSheet, Text, View } from "react-native";
-// import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-// import SignUpScreen from "./SignUpScreen";
-// // import RestPassword from './RestPassword';
-// // import LoginScreen from "./LoginScreen";
-
-// const cache = new InMemoryCache();
-// const client = new ApolloClient({
-//   uri: "http://192.168.0.88:8080/wbpro/wbpro",
-//   cache,
-//   defaultOptions: { watchQuery: { fetchPolicy: "cache-and-network" } },
-// });
-
-// export default function App() {
-//   return (
-//     <ApolloProvider client={client}>
-//       {/* <LoginScreen /> */}
-//       <View style={{ marginTop: 100 }}>
-//         <Text>APP Interface 3</Text>
-//         <SignUpScreen />
-//         {/* <LoginScreen /> */}
-//         {/* <RestPassword/> */}
-//       </View>
-//       <StatusBar />
-//     </ApolloProvider>
-//   );
-// }
 import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { NavigationContainer } from "@react-navigation/native";
